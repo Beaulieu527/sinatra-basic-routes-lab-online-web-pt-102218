280cd865db04cf1ce250800465694d690898ab27
Justin Beaulieu <beaulieudev@gmail.com>
2019-02-14 03:13:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    # some code to get all the medicines
+    # some code to render the correct HTML page
+ end
+ get '/hometown' do
+    # some code to get all the medicines
+    # some code to render the correct HTML page
+ end
+ get '/favorite-song' do
+    # some code to get all the medicines
+    # some code to render the correct HTML page
+ end
 end
